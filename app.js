9fa630e49c9d1c22d253eb148676c5b316211307
vikashparida1995 <vikash.parida1995@gmail.com>
2025-07-11 18:23:37 +0530

    create blocking API and non-blocking API

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
+
+
+
+function delay(duration ) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {
+        // Busy-wait loop to simulate delay
+    }
+
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
     }   
 );
 
+
+app.get('/timer', (req, res) => {
+//    setTimeout(()=>{
+//     return 
+//      },9000) ;
+delay(9000);
+return res.send('Hello lates  World!');
+}   
+);
 
 
 
